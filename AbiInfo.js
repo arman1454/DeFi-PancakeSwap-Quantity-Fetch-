cd78e748aaf5de7314fa70de2dcb0075b12eeb63
arman <fakidarman@gmail.com>
2024-05-31 06:46:28 -0400

    data fetching working

--- AbiInfo.js
+++ AbiInfo.js
@@ -2,7 +2,7 @@ const erc20ABI = ["function decimals() public view returns (uint8)"]
 
 // it will give that contrat address which is handling tokenA and tokenB (liquidity Provider contract)
 //0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16 liq provider address who provided BUSD WBNB
-const factoryABI = ["function getPair(address tokenA, address tokenB) external view returns (address pair);"]
+const factoryABI = ["function getPair(address tokenA, address tokenB) external view returns (address pair)"]
 
 const pairABI = [
     "function token0() external view returns (address)",
