6848af2c9ee80c148b78faedb7436aef055452ee
arman <fakidarman@gmail.com>
2024-05-31 02:42:39 -0400

    all the addressLists and the 3 ABIs

--- AddressList.js
+++ AddressList.js
@@ -1,7 +1,9 @@
-const factoryAddress="";
-const routerAddress="";
-const fromAddress=""; //BUSD
-const toAddress="";//WBNB
+//addreses of factory contract, router contract and those two tokens between 
+//which we are going to switch and the price of it for instance 1 Cake = 0.000 something BNB and the USD price
+const factoryAddress="0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";
+const routerAddress="0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const fromAddress="0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"; //BUSD. Basically first token address
+const toAddress="0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";//WBNB. 
 
 module.exports={
     factoryAddress,
